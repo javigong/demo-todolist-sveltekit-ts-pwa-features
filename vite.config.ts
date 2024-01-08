29369f2925b928b95f8d8c9dd22ebcd8994599be
Javier Gongora <42308135+javigong@users.noreply.github.com>
2024-01-07 20:47:53 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+  plugins: [sveltekit(), tsconfigPaths()],
+	test: {
+		alias: {
+			'@/': new URL('./src/', import.meta.url).pathname,
+		}
+	}
+})
